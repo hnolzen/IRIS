34bf812974c000992519070f5085885dae7ef960
Henning Nolzen <henning.nolzen@ufz.de>
2020-07-21 10:36:41 +0200

    initial commit

---
+++ src/test/java/eu/ecoepi/AppTest.java
@@ -0,0 +1,20 @@
+package eu.ecoepi;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
