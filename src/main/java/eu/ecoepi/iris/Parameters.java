4364eee3e4155822c173398fdf1bdc9794c271c3
Henning Nolzen <henning.nolzen@ufz.de>
2020-08-15 00:52:11 +0200

    Add daily temperature and humidity data of the year 2018 from the German Weather Service for a realistic representation of climate processes

--- src/main/java/eu/ecoepi/iris/Parameters.java
+++ src/main/java/eu/ecoepi/iris/Parameters.java
@@ -6,7 +6,7 @@ import java.util.Map;
 
 public class Parameters {
 
-    public static final int TIME_STEPS = 50;
+    public static final int TIME_STEPS = 365;
     public static final int GRID_WIDTH = 12;
     public static final int GRID_HEIGHT = 12;
     public static final int BOUNDARY_ECOTONE = 4;
@@ -16,17 +16,17 @@ public class Parameters {
     public static final float NYMPHS_TO_ADULTS = 0.3f;
 
     public static final float BIRTH_RATE = 0.5f;
-    public static final float DEATH_RATE = 0.25f;
+    public static final float DEATH_RATE = 0.3f;
 
     public static final Map<Habitat.Type, Float> DESICCATION_RATE = Map.of(
-            Habitat.Type.PASTURE, 0.3f,
-            Habitat.Type.ECOTONE, 0.2f,
-            Habitat.Type.WOOD, 0.1f
+            Habitat.Type.PASTURE, 0.35f,
+            Habitat.Type.ECOTONE, 0.25f,
+            Habitat.Type.WOOD, 0.2f
     );
 
     public static final Map<LifeCycleStage, Float> DISPERSAL_RATE = Map.of(
             LifeCycleStage.LARVAE, 0.005f,
-            LifeCycleStage.NYMPH, 0.02f,
-            LifeCycleStage.ADULT, 0.03f
+            LifeCycleStage.NYMPH, 0.05f,
+            LifeCycleStage.ADULT, 0.1f
     );
 }
--- src/main/java/eu/ecoepi/iris/systems/Weather.java
+++ src/main/java/eu/ecoepi/iris/systems/Weather.java
@@ -4,12 +4,18 @@ import com.artemis.ComponentMapper;
 import com.artemis.annotations.All;
 import com.artemis.annotations.Wire;
 import com.artemis.systems.IteratingSystem;
-import eu.ecoepi.iris.Parameters;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
+import com.opencsv.exceptions.CsvException;
 import eu.ecoepi.iris.Randomness;
 import eu.ecoepi.iris.components.Humidity;
 import eu.ecoepi.iris.components.Temperature;
 import eu.ecoepi.iris.TimeStep;
 import eu.ecoepi.iris.components.Habitat;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @All({Habitat.class})
 public class Weather extends IteratingSystem {
@@ -18,11 +24,32 @@ public class Weather extends IteratingSystem {
     ComponentMapper<Temperature> temperatureMapper;
     ComponentMapper<Humidity> humidityMapper;
 
+    final Map<Integer, Float> meanTempTimeSeries = new HashMap();
+    final Map<Integer, Float> minTempTimeSeries = new HashMap();
+    final Map<Integer, Float> maxTempTimeSeries = new HashMap();
+    final Map<Integer, Float> humidityTimeSeries = new HashMap();
+
     @Wire
     TimeStep timestep;
 
     @Wire
     Randomness randomness;
+
+    public Weather() throws IOException, CsvException {
+        CSVReader reader = new CSVReaderBuilder(new FileReader("weatherData.csv"))
+                .withSkipLines(1)
+                .build();
+        String[] nextLine;
+
+        int i = 0;
+        while ((nextLine = reader.readNext()) != null) {
+            meanTempTimeSeries.put(i, Float.parseFloat(nextLine[0]));
+            minTempTimeSeries.put(i, Float.parseFloat(nextLine[1]));
+            maxTempTimeSeries.put(i, Float.parseFloat(nextLine[2]));
+            humidityTimeSeries.put(i, Float.parseFloat(nextLine[3]));
+            ++i;
+        }
+    }
 
     @Override
     protected void process(int entityId) {
@@ -30,20 +57,18 @@ public class Weather extends IteratingSystem {
         var temperature = temperatureMapper.get(entityId);
         var humidity = humidityMapper.get(entityId);
 
-        // TODO: Insert real weather data
-        if (timestep.getCurrent() <= (Parameters.TIME_STEPS / 2)) {
-            temperature.setMeanTemperature(timestep.getCurrent());
-        } else {
-            temperature.setMeanTemperature(Parameters.TIME_STEPS - timestep.getCurrent());
-        }
+        temperature.setMeanTemperature(meanTempTimeSeries.get(timestep.getCurrent()));
+        temperature.setMinTemperature(minTempTimeSeries.get(timestep.getCurrent()));
+        temperature.setMaxTemperature(maxTempTimeSeries.get(timestep.getCurrent()));
+        humidity.setRelativeHumidity(humidityTimeSeries.get(timestep.getCurrent()));
 
-        if (habitat.getType() == Habitat.Type.PASTURE) {
-            humidity.setRelativeHumidity(50 + (randomness.random() * 50));
-        } else if (habitat.getType() == Habitat.Type.ECOTONE) {
-            humidity.setRelativeHumidity(55 + (randomness.random() * 45));
-        } else {
-            humidity.setRelativeHumidity(60 + (randomness.random() * 40));
-        }
+//        if (habitat.getType() == Habitat.Type.PASTURE) {
+//            humidity.setRelativeHumidity(humidityTimeSeries.get(timestep.getCurrent()));
+//        } else if (habitat.getType() == Habitat.Type.ECOTONE) {
+//            humidity.setRelativeHumidity(humidityTimeSeries.get(timestep.getCurrent()));
+//        } else {
+//            humidity.setRelativeHumidity(humidityTimeSeries.get(timestep.getCurrent()));
+//        }
 
     }
 }
