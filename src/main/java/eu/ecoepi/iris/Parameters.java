67a7b853c82625882838bf34b667862ef78be8b7
Henning Nolzen <henning.nolzen@ufz.de>
2020-08-17 00:05:40 +0200

    add weather dependent birth and death processes for more realistic tick population dynamics

--- src/main/java/eu/ecoepi/iris/Parameters.java
+++ src/main/java/eu/ecoepi/iris/Parameters.java
@@ -15,25 +15,43 @@ public class Parameters {
     public static final int INITIAL_LARVAE = 10;
     public static final int INITIAL_NYMPHS = 10;
     public static final int INITIAL_ADULTS = 10;
+
     public static final int INITIAL_INFECTED_LARVAE = 0;
     public static final int INITIAL_INFECTED_NYMPHS = 0;
     public static final int INITIAL_INFECTED_ADULTS = 0;
 
-    public static final float LARVAE_TO_NYMPHS = 0.4f;
-    public static final float NYMPHS_TO_ADULTS = 0.3f;
+    public static final float LARVAE_TO_NYMPHS = 0.5f;
+    public static final float NYMPHS_TO_ADULTS = 0.5f;
 
-    public static final float BIRTH_RATE = 0.5f;
-    public static final float DEATH_RATE = 0.3f;
+    public static final Map<Habitat.Type, Float> BIRTH_RATE_GOOD = Map.of(
+            Habitat.Type.PASTURE, 0.8f,
+            Habitat.Type.ECOTONE, 0.85f,
+            Habitat.Type.WOOD, 0.9f
+    );
+
+    public static final Map<Habitat.Type, Float> BIRTH_RATE_POOR = Map.of(
+            Habitat.Type.PASTURE, 0.25f,
+            Habitat.Type.ECOTONE, 0.3f,
+            Habitat.Type.WOOD, 0.35f
+    );
+
+    public static final float NATURAL_DEATH_RATE = 0.3f;
 
     public static final Map<Habitat.Type, Float> DESICCATION_RATE = Map.of(
-            Habitat.Type.PASTURE, 0.35f,
-            Habitat.Type.ECOTONE, 0.25f,
+            Habitat.Type.PASTURE, 0.5f,
+            Habitat.Type.ECOTONE, 0.4f,
             Habitat.Type.WOOD, 0.2f
     );
 
+    public static final Map<LifeCycleStage, Float> FREEZING_RATE = Map.of(
+            LifeCycleStage.LARVAE, 0.45f,
+            LifeCycleStage.NYMPH, 0.35f,
+            LifeCycleStage.ADULT, 0.35f
+    );
+
     public static final Map<LifeCycleStage, Float> DISPERSAL_RATE = Map.of(
-            LifeCycleStage.LARVAE, 0.005f,
-            LifeCycleStage.NYMPH, 0.05f,
-            LifeCycleStage.ADULT, 0.1f
+            LifeCycleStage.LARVAE, 0.3f,
+            LifeCycleStage.NYMPH, 0.45f,
+            LifeCycleStage.ADULT, 0.5f
     );
 }
--- src/main/java/eu/ecoepi/iris/systems/TickLifeCycle.java
+++ src/main/java/eu/ecoepi/iris/systems/TickLifeCycle.java
@@ -3,6 +3,7 @@ package eu.ecoepi.iris.systems;
 import com.artemis.ComponentMapper;
 import com.artemis.annotations.All;
 import com.artemis.systems.IteratingSystem;
+import eu.ecoepi.iris.LifeCycleStage;
 import eu.ecoepi.iris.Parameters;
 import eu.ecoepi.iris.components.Habitat;
 import eu.ecoepi.iris.components.Humidity;
@@ -24,24 +25,41 @@ public class TickLifeCycle extends IteratingSystem {
         var temperature = temperatureMapper.get(entityId);
         var humidity = humidityMapper.get(entityId);
 
-        var newLarvae = (int) (abundance.getAdults() * Parameters.BIRTH_RATE);
+        // Birth processes
+        var newLarvae = 0;
+        if (temperature.getMeanTemperature() > 5 && temperature.getMeanTemperature() < 19) {
+            newLarvae = (int) (abundance.getAdults() * Parameters.BIRTH_RATE_GOOD.get(habitat.getType()));
+        } else {
+            newLarvae = (int) (abundance.getAdults() * Parameters.BIRTH_RATE_POOR.get(habitat.getType()));
+        }
+
         var newNymphs = (int) (abundance.getLarvae() * Parameters.LARVAE_TO_NYMPHS);
         var newAdults = (int) (abundance.getNymphs() * Parameters.NYMPHS_TO_ADULTS);
 
-        var deadAdults = (int) (abundance.getAdults() * Parameters.DEATH_RATE);
+        // Death processes
+        var deadAdults = (int) (abundance.getAdults() * Parameters.NATURAL_DEATH_RATE);
 
         var desiccatedLarvae = 0;
         var desiccatedNymphs = 0;
         var desiccatedAdults = 0;
-        if (humidity.getRelativeHumidity() < 80.0) {
+        if (humidity.getRelativeHumidity() < 60.0 && temperature.getMeanTemperature() > 15.0 || temperature.getMaxTemperature() > 28) {
             desiccatedLarvae = (int) (abundance.getLarvae() * Parameters.DESICCATION_RATE.get(habitat.getType()));
             desiccatedNymphs = (int) (abundance.getNymphs() * Parameters.DESICCATION_RATE.get(habitat.getType()));
             desiccatedAdults = (int) (abundance.getAdults() * Parameters.DESICCATION_RATE.get(habitat.getType()));
         }
 
-        abundance.addLarvae(-newNymphs + newLarvae - desiccatedLarvae);
-        abundance.addNymphs(-newAdults + newNymphs - desiccatedNymphs);
-        abundance.addAdults(-deadAdults + newAdults - desiccatedAdults);
+        var freezedLarvae = 0;
+        var freezedNymphs = 0;
+        var freezedAdults = 0;
+        if (temperature.getMinTemperature() < 6.0) {
+            freezedLarvae = (int) (abundance.getLarvae() * Parameters.FREEZING_RATE.get(LifeCycleStage.LARVAE));
+            freezedNymphs = (int) (abundance.getNymphs() * Parameters.FREEZING_RATE.get(LifeCycleStage.NYMPH));
+            freezedAdults = (int) (abundance.getAdults() * Parameters.FREEZING_RATE.get(LifeCycleStage.ADULT));
+        }
+
+        abundance.addLarvae(-newNymphs + newLarvae - desiccatedLarvae - freezedLarvae);
+        abundance.addNymphs(-newAdults + newNymphs - desiccatedNymphs - freezedNymphs);
+        abundance.addAdults(-deadAdults + newAdults - desiccatedAdults - freezedAdults);
 
     }
 }
