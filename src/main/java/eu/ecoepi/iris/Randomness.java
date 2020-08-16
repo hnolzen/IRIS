4450d446d9a0c5c0435577366995f14733fd4e6b
Henning Nolzen <henning.nolzen@ufz.de>
2020-08-17 00:09:01 +0200

    add random dispersal for more tick population and landscape heterogeneity

--- src/main/java/eu/ecoepi/iris/Randomness.java
+++ src/main/java/eu/ecoepi/iris/Randomness.java
@@ -6,8 +6,12 @@ public class Randomness {
 
     final Random random = new Random(42);
 
-    public double random(){
+    public double random() {
         return this.random.nextDouble();
+    }
+
+    public int randomInt(int range) {
+        return this.random.nextInt(range);
     }
 
 }
--- src/main/java/eu/ecoepi/iris/systems/Dispersal.java
+++ src/main/java/eu/ecoepi/iris/systems/Dispersal.java
@@ -4,8 +4,8 @@ import com.artemis.ComponentMapper;
 import com.artemis.annotations.All;
 import com.artemis.annotations.Wire;
 import com.artemis.systems.IteratingSystem;
-import eu.ecoepi.iris.LifeCycleStage;
 import eu.ecoepi.iris.Parameters;
+import eu.ecoepi.iris.Randomness;
 import eu.ecoepi.iris.SpatialIndex;
 import eu.ecoepi.iris.components.Position;
 import eu.ecoepi.iris.components.TickAbundance;
@@ -19,34 +19,46 @@ public class Dispersal extends IteratingSystem {
     @Wire
     SpatialIndex index;
 
+    @Wire
+    Randomness randomness;
+
     @Override
     protected void process(int entityId) {
         var abundance = abundanceMapper.get(entityId);
         var position = positionMapper.get(entityId);
 
-        var neighbourToNorth = index.lookUp(position.moveBy(0, 1));
-        if (neighbourToNorth.isPresent()) {
-            var abundanceToNorth = abundanceMapper.get(neighbourToNorth.get());
-            processPair(abundance, abundanceToNorth);
+//        var neighbourToNorth = index.lookUp(position.moveBy(0, 1));
+//        if (neighbourToNorth.isPresent()) {
+//            var abundanceToNorth = abundanceMapper.get(neighbourToNorth.get());
+//            processPair(abundance, abundanceToNorth);
+//        }
+//
+//        var neighbourToSouth = index.lookUp(position.moveBy(0, -1));
+//        if (neighbourToSouth.isPresent()) {
+//            var abundanceToSouth = abundanceMapper.get(neighbourToSouth.get());
+//            processPair(abundance, abundanceToSouth);
+//        }
+//
+//        var neighbourToEast = index.lookUp(position.moveBy(1, 0));
+//        if (neighbourToEast.isPresent()) {
+//            var abundanceToEast = abundanceMapper.get(neighbourToEast.get());
+//            processPair(abundance, abundanceToEast);
+//        }
+//
+//        var neighbourToWest = index.lookUp(position.moveBy(-1, 0));
+//        if (neighbourToWest.isPresent()) {
+//            var abundanceToWest = abundanceMapper.get(neighbourToWest.get());
+//            processPair(abundance, abundanceToWest);
+//        }
+
+        var randomXDirection = randomness.randomInt(5 + 5) - 5;
+        var randomYDirection = randomness.randomInt(5 + 5) - 5;
+        var neighbourToRandom = index.lookUp(position.moveBy(randomXDirection, randomYDirection));
+        if (neighbourToRandom.isPresent()) {
+            var abundanceToRandom = abundanceMapper.get(neighbourToRandom.get());
+            processPair(abundance, abundanceToRandom);
         }
 
-        var neighbourToSouth = index.lookUp(position.moveBy(0, -1));
-        if (neighbourToSouth.isPresent()) {
-            var abundanceToSouth = abundanceMapper.get(neighbourToSouth.get());
-            processPair(abundance, abundanceToSouth);
-        }
-
-        var neighbourToEast = index.lookUp(position.moveBy(1, 0));
-        if (neighbourToEast.isPresent()) {
-            var abundanceToEast = abundanceMapper.get(neighbourToEast.get());
-            processPair(abundance, abundanceToEast);
-        }
-
-        var neighbourToWest = index.lookUp(position.moveBy(-1, 0));
-        if (neighbourToWest.isPresent()) {
-            var abundanceToWest = abundanceMapper.get(neighbourToWest.get());
-            processPair(abundance, abundanceToWest);
-        }
     }
 
     void processPair(TickAbundance abundance, TickAbundance neighbour) {
