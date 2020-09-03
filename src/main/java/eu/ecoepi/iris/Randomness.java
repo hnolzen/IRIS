fca2a86c4161138c3f4ed691b1947cebbc359aa1
Henning Nolzen <henning.nolzen@ufz.de>
2020-09-03 10:54:15 +0200

    explain rationale for random rounding

--- src/main/java/eu/ecoepi/iris/Randomness.java
+++ src/main/java/eu/ecoepi/iris/Randomness.java
@@ -14,6 +14,12 @@ public class Randomness {
         return this.random.nextInt(range);
     }
 
+    /*
+    This is necessary to compensate for the finite support of the model:
+    We only have a finite number of ticks but rates only make sense for
+    an infinite number of ticks. This method might round in the "wrong" direction.
+    This allows processes that statistically affect less than a single tick to still happen.
+     */
     public int roundRandom(float value) {
         int base = (int) value;
         float remainder = value - base;
