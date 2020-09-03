82bb505195d3880e1fcd1b32652aceab1c096ee7
Henning Nolzen <henning.nolzen@ufz.de>
2020-09-03 11:51:21 +0200

    outsource death, birth and development processes into individual methods for better clarity

--- src/main/java/eu/ecoepi/iris/systems/TickLifeCycle.java
+++ src/main/java/eu/ecoepi/iris/systems/TickLifeCycle.java
@@ -33,39 +33,43 @@ public class TickLifeCycle extends IteratingSystem {
         var humidity = humidityMapper.get(entityId);
         var precipitation = precipitationMapper.get(entityId);
 
+        development(abundance);
+        desiccation(abundance, habitat, temperature, humidity);
+        freezing(abundance, temperature, precipitation);
+        death(abundance);
+
+    }
+
+    private void development(TickAbundance abundance) {
         var nextStageLarvae = Parameters.INITIAL_NEXT_STAGE_LARVAE;
         var nextStageNymphs = Parameters.INITIAL_NEXT_STAGE_NYMPHS;
         var nextStageAdults = Parameters.INITIAL_NEXT_STAGE_ADULTS;
-
-        var desiccatedLarvae = Parameters.INITIAL_DESICCATED_LARVAE;
-        var desiccatedNymphs = Parameters.INITIAL_DESICCATED_NYMPHS;
-        var desiccatedAdults = Parameters.INITIAL_DESICCATED_ADULTS;
-
-        var frozenLarvae = Parameters.INITIAL_FROZEN_LARVAE;
-        var frozenNymphs = Parameters.INITIAL_FROZEN_NYMPHS;
-        var frozenAdults = Parameters.INITIAL_FROZEN_ADULTS;
-
-        var deadAdults = Parameters.INITIAL_DEAD_ADULTS;
 
         if (timestep.getCurrent() >= Parameters.BEGIN_OF_DEVELOPMENT && timestep.getCurrent() <= Parameters.END_OF_DEVELOPMENT) {
             nextStageLarvae = randomness.roundRandom(abundance.getFedAdults() * Parameters.ADULTS_TO_LARVAE);
             nextStageNymphs = randomness.roundRandom(abundance.getFedLarvae() * Parameters.LARVAE_TO_NYMPHS);
             nextStageAdults = randomness.roundRandom(abundance.getFedNymphs() * Parameters.NYMPHS_TO_ADULTS);
         }
+        abundance.addLarvae(nextStageLarvae);
+        abundance.addFedLarvae(-nextStageLarvae);
 
+        abundance.addNymphs(nextStageNymphs);
+        abundance.addFedNymphs(-nextStageNymphs);
+
+        abundance.addAdults(nextStageAdults);
+        abundance.addFedAdults(-nextStageAdults);
+    }
+
+    private void death(TickAbundance abundance) {
+        var deadAdults = Parameters.INITIAL_DEAD_ADULTS;
         deadAdults = randomness.roundRandom(abundance.getAdults() * Parameters.NATURAL_DEATH_RATE);
+        abundance.addAdults(-deadAdults);
+    }
 
-        var isDesiccated = false;
-        if (humidity.getRelativeHumidity() < 70 && temperature.getMeanTemperature() > 15) { // Ostfeld and Brunner 2015
-            isDesiccated = true;
-        }
-
-        if (isDesiccated) {
-            desiccatedLarvae = randomness.roundRandom(abundance.getLarvae() * Parameters.DESICCATION_RATE.get(habitat.getType()));
-            desiccatedNymphs = randomness.roundRandom(abundance.getNymphs() * Parameters.DESICCATION_RATE.get(habitat.getType()));
-            desiccatedAdults = randomness.roundRandom(abundance.getAdults() * Parameters.DESICCATION_RATE.get(habitat.getType()));
-        }
-
+    private void freezing(TickAbundance abundance, Temperature temperature, Precipitation precipitation) {
+        var frozenLarvae = Parameters.INITIAL_FROZEN_LARVAE;
+        var frozenNymphs = Parameters.INITIAL_FROZEN_NYMPHS;
+        var frozenAdults = Parameters.INITIAL_FROZEN_ADULTS;
         var isFrozen = false;
         if (temperature.getMinTemperature() < -18.9 ||                                            // Gray et al. 2009
                 (temperature.getMinTemperature() < -15.0 && precipitation.getSnowHeight() < 1)    // Ostfeld and Brunner 2015, Jore et al. 2014
@@ -79,13 +83,29 @@ public class TickLifeCycle extends IteratingSystem {
             frozenAdults = randomness.roundRandom(abundance.getAdults() * Parameters.FREEZING_RATE.get(LifeCycleStage.ADULT));
         }
 
-        abundance.addLarvae(nextStageLarvae - desiccatedLarvae - frozenLarvae);
-        abundance.addFedLarvae(-nextStageLarvae);
+        abundance.addLarvae(-frozenLarvae);
+        abundance.addNymphs(-frozenNymphs);
+        abundance.addAdults(-frozenAdults);
+    }
 
-        abundance.addNymphs(nextStageNymphs - desiccatedNymphs - frozenNymphs);
-        abundance.addFedNymphs(-nextStageNymphs);
+    private void desiccation(TickAbundance abundance, Habitat habitat, Temperature temperature, Humidity humidity) {
+        var desiccatedLarvae = Parameters.INITIAL_DESICCATED_LARVAE;
+        var desiccatedNymphs = Parameters.INITIAL_DESICCATED_NYMPHS;
+        var desiccatedAdults = Parameters.INITIAL_DESICCATED_ADULTS;
 
-        abundance.addAdults(-deadAdults + nextStageAdults - desiccatedAdults - frozenAdults);
-        abundance.addFedAdults(-nextStageAdults);
+        var isDesiccated = false;
+        if (humidity.getRelativeHumidity() < 70 && temperature.getMeanTemperature() > 15) { // Ostfeld and Brunner 2015
+            isDesiccated = true;
+        }
+
+        if (isDesiccated) {
+            desiccatedLarvae = randomness.roundRandom(abundance.getLarvae() * Parameters.DESICCATION_RATE.get(habitat.getType()));
+            desiccatedNymphs = randomness.roundRandom(abundance.getNymphs() * Parameters.DESICCATION_RATE.get(habitat.getType()));
+            desiccatedAdults = randomness.roundRandom(abundance.getAdults() * Parameters.DESICCATION_RATE.get(habitat.getType()));
+        }
+
+        abundance.addLarvae(-desiccatedLarvae);
+        abundance.addNymphs(-desiccatedNymphs);
+        abundance.addAdults(-desiccatedAdults);
     }
 }
