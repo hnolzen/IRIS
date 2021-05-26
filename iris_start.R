b7bfdcdc4cf8da33f6db4970806f42e4379f5f20
Henning Nolzen <henning.nolzen@ufz.de>
2021-05-26 17:56:18 +0200

    Create separate output directory for each climate model to distinguish between outputs of same years

--- iris_start.R
+++ iris_start.R
@@ -6,12 +6,17 @@ iris <- function(year,
                  activation_rate,
                  iris_main_dir,
                  weather_dir,
-                 output_summary
+                 output_summary,
+                 model_name
                  ) {
   
   iris_jar_dir <- paste0(iris_main_dir, "/target/")
   iris_input_dir <- paste0(iris_main_dir, "/input/")
-  output_dir <- paste0(iris_main_dir, "/output/")
+  output_dir <- paste0(iris_main_dir, "/output/", model_name)
+  
+  if (!dir.exists(output_dir)){
+    dir.create(output_dir, showWarnings = FALSE)
+  }
   
   iris_exe <- "java.exe"
   iris_jar <- paste0("-jar ", iris_jar_dir, "IRIS-1.0-SNAPSHOT-jar-with-dependencies.jar eu.ecoepi.iris.experiments.AdHocSimulation")
@@ -41,7 +46,7 @@ iris <- function(year,
 
 iris_main_dir <- dirname(rstudioapi::getSourceEditorContext()$path)
 location <- "regensburg"
-year_start <- 2018
+year_start <- 2021
 year_end <- 2099
 climate_simulation <- TRUE
 random_seed <- 42
@@ -53,7 +58,7 @@ output_summary <- "true"
 weather_dir <- paste0(iris_main_dir, "/input/weather/dwd_", location, "/")
 
 if (climate_simulation) {
-  model_name <- "All_models_daily_mean"
+  #model_name <- "All_models_daily_mean"
   #model_name <- "CCCma-CanESM2_rcp85_r1i1p1_CLMcom-CCLM4-8-17_v1"
   #model_name <- "CCCma-CanESM2_rcp85_r1i1p1_GERICS-REMO2015_v1"
   #model_name <- "IPSL-IPSL-CM5A-MR_rcp85_r1i1p1_IPSL-WRF381P_v1"
@@ -63,14 +68,14 @@ if (climate_simulation) {
   #model_name <- "MOHC-HadGEM2-ES_rcp85_r1i1p1_DMI-HIRHAM5_v2"
   #model_name <- "MOHC-HadGEM2-ES_rcp85_r1i1p1_GERICS-REMO2015_v1"
   #model_name <- "MOHC-HadGEM2-ES_rcp85_r1i1p1_ICTP-RegCM4-6_v1"
-  #model_name <- "MOHC-HadGEM2-ES_rcp85_r1i1p1_IPSL-WRF381P_v1"
-  #model_name <- "MOHC-HadGEM2-ES_rcp85_r1i1p1_KNMI-RACMO22E_v2"
   #model_name <- "MOHC-HadGEM2-ES_rcp85_r1i1p1_MOHC-HadREM3-GA7-05_v1"
   #model_name <- "MOHC-HadGEM2-ES_rcp85_r1i1p1_SMHI-RCA4_v1"
   #model_name <- "MPI-M-MPI-ESM-LR_rcp85_r3i1p1_GERICS-REMO2015_v1"
   model_name <- "MPI-M-MPI-ESM-LR_rcp85_r3i1p1_SMHI-RCA4_v1"
   
   weather_dir <- paste0(iris_main_dir, "/input/climate/", model_name, "/csv_", location, "/")  
+} else {
+  model_name <- "DWD"
 }
 
 for (year in year_start : year_end) {
@@ -82,5 +87,6 @@ for (year in year_start : year_end) {
        activation_rate,
        iris_main_dir,
        weather_dir,
-       output_summary) 
+       output_summary,
+       model_name) 
 }
