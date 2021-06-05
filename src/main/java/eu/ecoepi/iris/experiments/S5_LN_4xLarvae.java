3e16870fba1890db8b2a245868d3cbe01d6c517c
Henning Nolzen <henning.nolzen@ufz.de>
2021-06-05 21:34:55 +0200

    Update validation report with latest validation results

---
+++ src/main/java/eu/ecoepi/iris/experiments/S5_LN_4xLarvae.java
@@ -0,0 +1,34 @@
+package eu.ecoepi.iris.experiments;
+
+import eu.ecoepi.iris.Model;
+
+public class S5_LN_4xLarvae {
+    public static void main(String[] args) throws Exception {
+        var large = new Large();
+
+        for (int year = 2009; year <= 2018; year++) {
+            var weather = String.format("./input/weather/dwd_regensburg/weather_%d.csv", year);
+
+            for (int ticks = 2; ticks <= 500; ticks += 2) {
+                for (int activationRate = 10; activationRate <= 30; activationRate += 1) {
+                    var options = new Model.Options();
+
+                    var name = String.format("%d_%d_%d", year, ticks, activationRate);
+
+                    options.weather = weather;
+                    options.output = String.format("./output/sensitivity_analysis_%s.csv", name);
+
+                    options.initialInactiveLarvae = ticks * 4;
+                    options.initialInactiveNymphs = ticks;
+                    options.initialInactiveAdults = ticks;
+
+                    options.activationRate = activationRate / 1000.0f;
+
+                    large.addTask(name, options);
+                }
+            }
+        }
+
+        large.waitForCompletion();
+    }
+}
